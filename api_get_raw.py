d636e9625f2be2e89525282ba9baf68508b8092e
Pustovit Vladislav <pvs013@campus.mephi.ru>
2024-01-16 23:39:50 +0300

    fix

--- api_get_raw.py
+++ api_get_raw.py
@@ -1,13 +1,12 @@
 from github import Github, Auth
 
 
-def get_raw_github_repo_csv(repo_name: str, token: str, ignore_files: list[str], csv_file: str) -> None:
+def get_raw_github_repo_csv(repo_name: str, token: str, ignore_files: list[str], csv_file: str, is_malware: int) -> None:
     auth = Auth.Token(token)
     g = Github(auth=auth)
     repo = g.get_repo(repo_name)
     content = repo.get_contents("")
     with open(csv_file, "a") as malicious_csv:
-        malicious_csv.write("filename,url,isMalicious\n")
         while content:
             file_content = content.pop(0)
             if file_content.type == "dir":
@@ -17,4 +16,4 @@ def get_raw_github_repo_csv(repo_name: str, token: str, ignore_files: list[str],
                 if exe in ignore_files:
                     continue
                 malicious_csv.write(
-                    f"{file_content.path},https://raw.githubusercontent.com/{repo_name}/main/{file_content.path},1\n")
+                    f"{file_content.path},https://raw.githubusercontent.com/{repo_name}/main/{file_content.path},{is_malware}\n")
--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from config import TOKEN
 def main():
     repo = input("Enter GitHub repository likes 'OWNER/REPOSITORY_NAME': ")
     csv_file = input("Enter path to csv file: ")
+    is_malware = int(input("Enter 0 if repo is legal and 1 for malware: "))
     archive = [".7z", ".zip", ".tar.gz", ".tgz", ".rar"]
     other_file = [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tif", ".tiff", ".md", ".asm", ".txt"]
     useless_file = archive + other_file
-    get_raw_github_repo_csv(repo, TOKEN, useless_file, csv_file)
+    get_raw_github_repo_csv(repo, TOKEN, useless_file, csv_file, is_malware)
 
 
 if __name__ == "__main__":
